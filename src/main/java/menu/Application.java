e6bd68724aef3e3fc6c4935844b5454484f6ab38
zakie <realzzaekkii@gmail.com>
2026-01-02 23:23:58 +0900

    chore: 이전 참여자 작성 코드 삭제

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -6,7 +6,5 @@ import menu.model.service.RecommendService;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        RecommendController controller = new RecommendController(new RecommendService());
-        controller.start();
     }
 }
